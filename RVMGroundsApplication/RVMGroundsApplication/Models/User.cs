f59b354b93ea52b814f8ad8d833af0287c81313f
Jareth Dodson <jareth.dodson@oit.edu>
2023-02-06 18:22:19 -0800

    Added User class

---
+++ RVMGroundsApplication/RVMGroundsApplication/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace RVMGroundsApplication.Models
+{
+	class User
+    {
+        string username { get; set; }
+        string password { get; set; }
+        string hash { get; set; }
+        string name { get; set; }
+        long phoneNum { get; set; }
+        string row { get; set; }
+        string jobTitle { get; set; }
+        bool isAdmin { get; set; }
+    }
+}
